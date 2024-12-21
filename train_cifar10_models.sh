28ce59e6018706236b0fecf03ac1bbc0ba4e07e5
Yaoyu <jinyaoyu2001754@gmail.com>
2024-12-21 21:17:29 +0800

    ignore file sample

--- train_cifar10_models.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-for dp in 0.0
-do
-python train_cifar10_models.py --train-dp $dp --gpu $1 --net $2 --seed 2020 --sidx 1 --eidx 2400
-done
-python train_cifar10_models.py --net SENet18 --train-dp 0.2 --gpu 0
